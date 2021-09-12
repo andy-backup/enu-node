0599da81e78d81b071fa75e33b7e48f5f762b96b
Mingfun Wong <mingfun.wong.chn@gmail.com>
2021-09-12 08:25:05 +0000

    update

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 SNAPSHOT_FOLDER=./data/snapshots
 SNAPSHOT_FILE=$(ls $SNAPSHOT_FOLDER -t | head -1)
 
-rm -rf ./data/blocks ./data/state
-
-find $SNAPSHOT_FOLDER/* -mtime +30
-find $SNAPSHOT_FOLDER/* -mtime +30 -delete
+rm -rf ./data/blocks ./data/state ./data/snapshots/*
+URL=https://github.com/andy-backup/enu/raw/master/snapshot.bin
+wget $URL -P $SNAPSHOT_FOLDER
 
 ./enunode --data-dir ./data --config-dir ./config --snapshot $SNAPSHOT_FOLDER/$SNAPSHOT_FILE
